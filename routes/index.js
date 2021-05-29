223e4f6d9b95f887aedfec699e77153e147984db
Lewis Lu <agooday32@gmail.com>
2021-05-29 15:46:50 +0800

    feat: add user login/register page

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
-router.use('/', home)
+
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
+router.use('/', home)
 
 module.exports = router
