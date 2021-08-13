e92430217e11028289aaa3d02febdd7690dc923c
lewislu327 <agooday32@gmail.com>
2021-08-13 11:37:49 +0800

    feat: add handlebars-helpers, add show alert message in creating new restaurant page

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const app = express()
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override') 
+const exphbs = require('express-handlebars')
+const hbshelpers = require('handlebars-helpers')
+const multihelpers = hbshelpers()
+const methodOverride = require('method-override')
 const session = require('express-session')
 const port = process.env.PORT
 const routes = require('./routes')
 require('./config/mongoose')
 const usePassport = require('./config/passport')
-const flash = require('connect-flash') 
+const flash = require('connect-flash')
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main', helpers: multihelpers }))
 app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
@@ -32,7 +35,7 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg') 
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -7,16 +7,31 @@ router.get('/new', (req, res) => {
 })
 router.post('/new', (req, res) => {
   const userId = req.user._id
-  const name = req.body.name.trim() 
+  const name = req.body.name.trim()
   const { category, location, phone, rating, image, google_map, description } = req.body
+  const errors = []
 
-  if (name.length === 0){
-    res.send('please enter valid word') 
+  if (!name || category === '選擇餐廳類型') {
+    errors.push({ message: '餐廳名稱&類型為必填欄位。' })
+  }
+
+  if (errors.length) {
+    return res.render('new', {
+      errors,
+      name,
+      category,
+      image,
+      location,
+      phone,
+      google_map,
+      rating,
+      description
+    })
   }
 
   return Restaurant.create({ name, category, location, phone, rating, image, google_map, description, userId })
-  .then(() => res.redirect('/')) 
-  .catch(error => console.log(error))
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
 })
 
 // route for detail page
@@ -26,7 +41,7 @@ router.get('/:id', (req, res) => {
   return Restaurant.findOne({ _id, userId })
     .lean()
     .then((restaurant) => res.render('detail', { restaurant }))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error))
 })
 
 //route for edit page
@@ -34,33 +49,28 @@ router.get('/:id/edit', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
   return Restaurant.findOne({ _id, userId })
-  .lean()
-  .then((restaurant) => res.render('edit', { restaurant }))
-  .catch(error => console.log(error))
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
   const userId = req.user._id
-  const _id = req.params.id 
+  const _id = req.params.id
   return Restaurant.findOne({ _id, userId })
-  .then( restaurant => {
-    restaurant.name = req.body.name.trim();
-    restaurant.category = req.body.category
-    restaurant.image = req.body.image
-    restaurant.location = req.body.location
-    restaurant.phone = req.body.phone
-    restaurant.rating = req.body.rating
-    restaurant.google_map = req.body.google_map
-    restaurant.description = req.body.description
-
-    if (restaurant.name.length === 0 ) {
-      res.send('please enter valid name')
-    }
-
-    return restaurant.save()
-  })
-  .then( () => res.redirect(`/restaurants/${_id}`))
-  .catch( error => console.log(error))
+    .then((restaurant) => {
+      restaurant.name = req.body.name.trim()
+      restaurant.category = req.body.category
+      restaurant.image = req.body.image
+      restaurant.location = req.body.location
+      restaurant.phone = req.body.phone
+      restaurant.rating = req.body.rating
+      restaurant.google_map = req.body.google_map
+      restaurant.description = req.body.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${_id}`))
+    .catch((error) => console.log(error))
 })
 
 //route for delete item
@@ -68,10 +78,9 @@ router.delete('/:id/', (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
   return Restaurant.findOne({ _id, userId })
-    .then(restaurant => restaurant.remove())
+    .then((restaurant) => restaurant.remove())
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error))
 })
-
 
 module.exports = router
