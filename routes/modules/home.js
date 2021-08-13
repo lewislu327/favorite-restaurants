d7d370fe622e5f2ffee11d27b6c29a65c90840b5
lewislu327 <agooday32@gmail.com>
2021-08-13 11:38:28 +0800

    refactor: modify search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
-// route for home page 
+// route for home page
 router.get('/', (req, res) => {
-  const userId = req.user._id;
-  Restaurant.find({ userId })
+  const userId = req.user._id
+  const keyword = req.query.keyword
+  if (keyword) {
+    return Restaurant.find({
+      userId: userId,
+      $or: [
+        { name: { $regex: keyword, $options: '$i' } },
+        { category: { $regex: keyword, $options: '$i' } }
+      ]
+    })
+      .lean()
+      .sort({ name: 'asc' })
+      .then((restaurant) => res.render('index', { restaurant, keyword }))
+      .catch((error) => console.error(error))
+  }
+
+  return Restaurant.find({ userId })
     .lean()
-    .sort({ name: 'asc'})
-    .then(restaurant => res.render('index', { restaurant: restaurant }))
-    .catch(error => console.error(error))
+    .sort({ name: 'asc' })
+    .then((restaurant) => res.render('index', { restaurant }))
+    .catch((error) => console.error(error))
 })
-
-
 
 module.exports = router
